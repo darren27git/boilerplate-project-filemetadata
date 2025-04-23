18fdf9dd13a4fa23117070dfdbc747934789c44d
Darren L <lgytuan@gmail.com>
2025-04-23 07:41:24 +0000

    done

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer = require('multer');
+
+const upload = multer({dest : 'upload/'});
 
 var app = express();
 
@@ -12,7 +15,12 @@ app.get('/', function (req, res) {
 });
 
 
-
+app.post('/api/fileanalyse', upload.single('upfile'),(req,res) => {
+  if(!req.file) {
+    return res.status(400).send('No file found.');
+  }
+  res.json({name: req.file.originalname, type: req.file.mimetype, size : req.file.size});
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
